112086816a5c775d116fb633cd7e2aeaaa38c7f0
Praveen Kumaravel <praveen1994kp@gmail.com>
2019-07-22 23:11:09 +0530

    Added complete webpack setup for react app with styling and hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const buildValidations = require("./build-utils/build-validations");
+const commonConfig = require("./build-utils/webpack.common");
 
-const plugins = [
-  new HtmlWebpackPlugin({
-    template: "./src/index.html",
-    filename: "./index.html"
-  })
-];
+const webpackMerge = require("webpack-merge");
 
-const config = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  },
-  plugins: plugins
+const addons = (/* string | string[] */ addonsArg) => {
+  let addons = [...[addonsArg]] // Normalize array of addons (flatten)
+    .filter(Boolean); // If addons is undefined, filter it out
+
+  return addons.map(addonName =>
+    require(`./build-utils/addons/webpack.${addonName}.js`)
+  );
 };
 
-module.exports = config;
+module.exports = env => {
+  if (!env) {
+    throw new Error(buildValidations.ERR_NO_ENV_FLAG);
+  }
+
+  const envConfig = require(`./build-utils/webpack.${env.env}.js`);
+  const mergedConfig = webpackMerge(
+    commonConfig,
+    envConfig,
+    ...addons(env.addons)
+  );
+
+  return mergedConfig;
+};
